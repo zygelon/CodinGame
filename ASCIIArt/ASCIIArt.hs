1b3f9a13f2968f431a1a843a2d587969827c3ccb
Maksym Bondar <mbondar@stalker2.com>
2021-10-27 01:52:37 +0300

    added foldr

--- ASCIIArt/ASCIIArt.hs
+++ ASCIIArt/ASCIIArt.hs
@@ -1,53 +1,54 @@
-﻿import System.IO
-import Control.Monad
+﻿import Control.Monad
 import Data.Char
+import System.IO
 
-data AlphaData = AlphaData {lineLength :: Int
-                           , letters :: [String]}
+data AlphaData = AlphaData
+  { lineLength :: Int,
+    letters :: [String]
+  }
 
 convertSymbToNum symbol =
-    if isAlpha symbol
+  if isAlpha symbol
     then
-        let upSymbol = toUpper symbol
-        in ord upSymbol -  ord 'A'
+      let upSymbol = toUpper symbol
+       in ord upSymbol - ord 'A'
     else 1 + ord 'Z' - ord 'A'
 
 getFullLetter :: AlphaData -> Int -> [String]
 getFullLetter letterNum alphaData =
-    let alphaDataNextIter :: Int -> AlphaData -> [String]
-        alphaDataNextIter _ (AlphaData _ []) = []
-        alphaDataNextIter letterNum (AlphaData l (x:xs)) =
-           let chopedLeft = drop (letterNum * l) x
-           in  take l chopedLeft : alphaDataNextIter letterNum (AlphaData l xs)
-    in alphaDataNextIter alphaData letterNum
+  let alphaDataNextIter :: Int -> AlphaData -> [String]
+      alphaDataNextIter _ (AlphaData _ []) = []
+      alphaDataNextIter letterNum (AlphaData l (x : xs)) =
+        let chopedLeft = drop (letterNum * l) x
+         in take l chopedLeft : alphaDataNextIter letterNum (AlphaData l xs)
+   in alphaDataNextIter alphaData letterNum
 
-letterToStr :: [String] -> String
-letterToStr [] = []
-letterToStr (x:xs) =
-    x ++ ['\n'] ++ letterToStr xs
+lettersToOutFormat :: [String] -> String
+lettersToOutFormat [] = []
+lettersToOutFormat (x : xs) =
+  x ++ ['\n'] ++ lettersToOutFormat xs
 
-convertWord alphaData [] = []
-convertWord alphaData (x:xs) =
-    getFullLetter alphaData (convertSymbToNum x) +++ (convertWord alphaData xs)
+convertWord alphaData list
+  = foldr ((+++) . getFullLetter alphaData . convertSymbToNum) [] list
 
 (+++) a b | null a = b
 (+++) a b | null b = a
-(+++) (x1:xs1) (x2:xs2) = (x1 ++ x2) : (xs1 +++ xs2)
+(+++) (x1 : xs1) (x2 : xs2) = (x1 ++ x2) : (xs1 +++ xs2)
 
 main :: IO ()
 main = do
-    hSetBuffering stdout NoBuffering -- DO NOT REMOVE
+  hSetBuffering stdout NoBuffering -- DO NOT REMOVE
 
-    -- Auto-generated code below aims at helping you parse
-    -- the standard input according to the problem statement.
+  -- Auto-generated code below aims at helping you parse
+  -- the standard input according to the problem statement.
 
-    input_line <- getLine
-    let l = read input_line :: Int
-    input_line <- getLine
-    let h = read input_line :: Int
-    wordToConvert <- getLine
+  input_line <- getLine
+  let l = read input_line :: Int
+  input_line <- getLine
+  let h = read input_line :: Int
+  wordToConvert <- getLine
 
-    alphabet <- replicateM h getLine
-    putStrLn $ letterToStr $ convertWord (AlphaData l alphabet) wordToConvert
-    -- hPutStrLn stderr "Debug messages..."
-    return ()
+  alphabet <- replicateM h getLine
+  putStrLn . lettersToOutFormat $ convertWord (AlphaData l alphabet) wordToConvert
+  -- hPutStrLn stderr "Debug messages..."
+  return ()
