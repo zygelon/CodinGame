1c2524e3baa5155108ac1c5a69cb31d4fc47b963
zygelon <zygelon@gmail.com>
2021-10-28 04:32:27 +0300

    Resistance WIP

--- Resistance/Resistance.hs
+++ Resistance/Resistance.hs
@@ -5,6 +5,10 @@ import Data.Either
 import Data.List (elemIndex, find)
 import Numeric
 import Text.Printf
+import Test.QuickCheck
+
+prop_reverse :: [Int] -> Bool
+prop_reverse xs = reverse (reverse xs) == xs
 {-readInput :: Int -> Int -> IO [String]
 readInput currResist numResists = do
     if currResist >= numResists
@@ -27,11 +31,11 @@ bracketToCalcType "(" = Just ESeries
 bracketToCalcType _ = Nothing
 isOpenBracket bracket = bracket == "(" || bracket == "["
 isCloseBracket bracket = bracket == ")" || bracket == "]"
-
+{-
 getOpenBracketFunc mainFunc calcType resistData [] = Nothing --(0.0, [])
 getOpenBracketFunc mainFunc calcType resistData (x:xs) | x == "[" = calcResistMain EParallel resistData xs
 getOpenBracketFunc mainFunc calcType resistData (x:xs) | x == "(" = calcResistMain ESeries resistData xs
-
+-}
 
 getResistance :: String -> [(String, Float)] -> Float
 getResistance resistorName resistData =
@@ -48,7 +52,7 @@ calcResistMain _ _ (x:xs) | isCloseBracket x = Nothing
 
 calcResistMain calcType resistData (x:xs) | isOpenBracket x =
     let Just innerCalcType = bracketToCalcType x
-        Just (resistFloat, list) = getOpenBracketFunc calcResistMain innerCalcType resistData (x:xs)
+        Just (resistFloat, list) = calcResistMain innerCalcType resistData (x:xs)
         --resistFloat = getResistance resistance resistData
         resistFloatCalcTyped = if innerCalcType == ESeries then resistFloat else 1.0 / resistFloat
         Just (nextResistance, list2) = calcResistMain calcType resistData list
@@ -62,14 +66,19 @@ calcResistMain calcType resistData (x:xs) =
 
 activate :: [(String, Float)] -> [String] -> Maybe (Float, [String])
 activate resistData (x:xs) =
-    if | x == "[" = calcResistMain EParallel resistFloat xs 
+    case x of
+        "[" -> let Just (resistFloat, str) = calcResistMain EParallel resistData xs
+               in Just (1.0 / resistFloat, str)
+        "(" -> calcResistMain ESeries resistData xs
+        _   -> Nothing
+    {-if | x == "[" = calcResistMain EParallel resistFloat xs 
        | x == "(" = calcResistMain ESeries resistFloat xs
-       | otherwise = Nothing
+       | otherwise = Nothing-}
 
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering -- DO NOT REMOVE
-
+    quickCheck prop_reverse
     -- Auto-generated code below aims at helping you parse
     -- the standard input according to the problem statement.
 
@@ -82,9 +91,12 @@ main = do
     --print "______________________"
     let resistsData = convertRawToResistData rawResistsData
     --print $ show resistsData
-    let Just (result, _) = activate resistsData (words rawResistLinkStr)
+    --let Just (result, _) = calcResistMain ESeries resistsData (words rawResistLinkStr)
+    let Just (result, str) = activate resistsData (words rawResistLinkStr)
     -- hPutStrLn stderr "Debug messages..."
-    let outStr _ = showFFloat (Just 10) result
+--    print $ show result
+--    print $ show str
+   -- let outStr _ = showFFloat (Just 10) result
     printf "%.1f\n" $ result
     --putStrLn "Equivalent Resistance"
 {- Test
