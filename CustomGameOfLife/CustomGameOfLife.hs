a10343e471f3ba24a1921d2c62eb7478cf2874b8
Maksym Bondar <mbondar@stalker2.com>
2021-10-31 22:55:13 +0300

    Custom Game Of Live wip

--- CustomGameOfLife/CustomGameOfLife.hs
+++ CustomGameOfLife/CustomGameOfLife.hs
@@ -1,5 +1,7 @@
 ﻿import System.IO
 import Control.Monad
+--NEED POLISHING THIS FORMAT
+--012345678
 --001100000
 --000100000 conway
 setupLen = 9
@@ -7,22 +9,79 @@ setupLen = 9
 data CellSetup = CellSetup { alive :: [Bool]
                            , dead  :: [Bool] }
 
-newtype World = World [[Bool]]
+type World = [[Bool]]
 
 strWCellToBool '.' = False
 strWCellToBool 'O' = True
+boolWCellToStr True  = 'O'
+boolWCellToStr False = '.'
 
-makeBoolFromStrWorld :: [String] -> [[Bool]]
-makeBoolFromStrWorld (x:xs) = fmap strWCellToBool x : makeBoolFromStrWorld xs
+
+makeBoolFromStrWorld :: [String] -> World
+makeBoolFromStrWorld list = map (map strWCellToBool) list
+
+makeStrFromBoolWorld :: World -> [String]
+makeStrFromBoolWorld list = map (map boolWCellToStr) list
 
 strSetupToBool '1' = True
 strSetupToBool '0' = False
 
 inputToWSetup :: String -> String -> CellSetup
 inputToWSetup alive dead | length alive == setupLen && length dead == setupLen =
-    let aliveSetup = fmap strSetupToBool alive
-        deadSetup = fmap strSetupToBool dead
+    let aliveSetup = map strSetupToBool alive
+        deadSetup = map strSetupToBool dead
     in CellSetup aliveSetup deadSetup
+
+getCell :: World -> Int -> Int -> Bool
+getCell _ x y                                          | x < 0 || y < 0 = False
+getCell world x y       | y >= length world || x >= length (world !! 0) = False
+getCell world x y                                                       = world !! y !! x
+
+getSumAliveNeighbours :: World -> Int -> Int -> Int
+getSumAliveNeighbours world cellX cellY =
+    let neighbors = [[-1, -1], --0
+                     [-1,  0], --1
+                     [-1,  1], --2
+                     [ 0, -1], --3
+                     [ 0,  1], --4
+                     [ 1, -1], --5
+                     [ 1,  0], --6
+                     [ 1,  1]] --7
+        cellStatus = \[rely, relx] -> getCell world (cellX + relx) (cellY + rely)
+        neighbStates = fmap cellStatus neighbors
+    in foldl (\bFirstVal bSecondVal -> fromEnum bFirstVal + fromEnum bSecondVal) 0 neighbStates
+
+getCellNextGeneration :: CellSetup -> World -> Int -> Int -> Bool
+getCellNextGeneration (CellSetup alive dead) world cellX cellY =
+    let aliveNeighbors = getSumAliveNeighbours world cellX cellY
+        curCellStatus = world !! cellY !! cellX
+    in if curCellStatus
+       then dead !! aliveNeighbors -- alive -> dead transition ?
+       else alive !! aliveNeighbors -- dead -> alive transition ?
+
+getWorldNextGeneration :: CellSetup -> World -> World
+getWorldNextGeneration cellSetup world =
+    let height = length world
+        width = length $ head world
+        wNextGenAllLines :: Int -> World
+        wNextGenAllLines y | y >= height = []
+        wNextGenAllLines y               =
+            let wNextGenLine :: Int -> Int -> [Bool]
+                wNextGenLine x y =
+                    let result = getCellNextGeneration cellSetup world x y
+                    in if x < width then result : wNextGenLine (x + 1) y
+                       else []
+            in wNextGenLine 0 y : wNextGenAllLines (y + 1)
+    in wNextGenAllLines 0
+
+getWorldNGeneration :: CellSetup -> World -> Int -> World
+getWorldNGeneration cellSetup world n =
+    let getWorldNGenerationRec :: World -> Int -> World
+        getWorldNGenerationRec currWorld currIter | currIter >= n = currWorld
+        getWorldNGenerationRec currWorld currIter =
+            let nextWGener = getWorldNextGeneration cellSetup world
+            in getWorldNGenerationRec nextWGener (currIter + 1)
+    in getWorldNGenerationRec world 0
 
 main :: IO ()
 main = do
@@ -43,9 +102,21 @@ main = do
 
     worldStr <- replicateM h $ do getLine
 
-    let world = World $ makeBoolFromStrWorld worldStr
+    let cellSetup = inputToWSetup alive dead
+    let world = makeBoolFromStrWorld worldStr
+    let nGeneration = getWorldNGeneration cellSetup world n
+    let strOutWorld = makeStrFromBoolWorld nGeneration
+    let outFormatWGeneration = foldr (++) "" (map (\str -> str ++ "\n") strOutWorld)
     -- hPutStrLn stderr "Debug messages..."
-
+    hPutStrLn stderr $ show world
+    let debugWorld = getWorldNextGeneration cellSetup world
+    hPutStrLn stderr "height"
+    hPutStrLn stderr $ show (length world)
+    hPutStrLn stderr "width"
+    hPutStrLn stderr $ show (length $ head world)
+    hPutStrLn stderr "debugWorld"
+    hPutStrLn stderr $ show debugWorld
+    hPutStrLn stderr (show nGeneration)
     -- Write answer to stdout
-    putStrLn "grid"
+    putStrLn outFormatWGeneration
     return ()
